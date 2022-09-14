ef88ecb0560d725de47aceb7d4980ba200426930
ayumi nakano <aynminoa@gmail.com>
2022-09-14 17:17:04 +0900

    カラム追加、ストロングパラメータ設定、application.html.erb変更、ログイン画面変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_073238) do
+ActiveRecord::Schema.define(version: 2022_09_14_074413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 2022_09_14_073238) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "self_introduction"
+    t.string "image"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
