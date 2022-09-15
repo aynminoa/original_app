a37db9afb50371bd114c4dc5de0ed0602375da2c
ayumi nakano <aynminoa@gmail.com>
2022-09-15 15:54:23 +0900

    user詳細画面でalbum投稿を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show' => "users#show"
   resources :spots
-  devise_for :users
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' }
+  # get 'users/:id' => "users#show"
+  resources :users, only: %i[show]
   resources :albums
   root 'albums#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
