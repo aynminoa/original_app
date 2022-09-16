0af30559d5f8ac28aad226b81d9b36dc5ffa51b0
ayumi nakano <aynminoa@gmail.com>
2022-09-16 21:04:45 +0900

    spot詳細に地図表示実装

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,73 +1,50 @@
 class SpotsController < ApplicationController
   before_action :set_spot, only: %i[ show edit update destroy ]
 
-  # GET /spots or /spots.json
   def index
     @spots = Spot.all
   end
 
-  # GET /spots/1 or /spots/1.json
   def show
+    gon.spot = @spot
   end
 
-  # GET /spots/new
   def new
     @spot = Spot.new
     @album = Album.find_by(id: params[:album_id])
   end
   
-  # GET /spots/1/edit
   def edit
   end
   
-  # POST /spots or /spots.json
   def create
     @spot = Spot.new(spot_params)
-    
-    # @spot = @album.spots.build(spot_params)
-
-    respond_to do |format|
-      if @spot.save
-        format.html { redirect_to spot_url(@spot), notice: "Spot was successfully created." }
-        format.json { render :show, status: :created, location: @spot }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @spot.errors, status: :unprocessable_entity }
-      end
+    if @spot.save
+      redirect_to spot_url(@spot), notice: "Spot was successfully created."
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /spots/1 or /spots/1.json
   def update
-    respond_to do |format|
-      if @spot.update(spot_params)
-        format.html { redirect_to spot_url(@spot), notice: "Spot was successfully updated." }
-        format.json { render :show, status: :ok, location: @spot }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @spot.errors, status: :unprocessable_entity }
-      end
+    if @spot.update(spot_params)
+      redirect_to spot_url(@spot), notice: "Spot was successfully updated."
+    else
+      render :edit
     end
   end
 
-  # DELETE /spots/1 or /spots/1.json
   def destroy
     @spot.destroy
-
-    respond_to do |format|
-      format.html { redirect_to spots_url, notice: "Spot was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      redirect_to spots_url, notice: "Spot was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_spot
       @spot = Spot.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def spot_params
-      params.require(:spot).permit(:name, :address, :visited_on, :url, :comment, :album_id)
+      params.require(:spot).permit(:name, :address, :visited_on, :url, :comment, :latitude, :longitude, :album_id)
     end
 end
