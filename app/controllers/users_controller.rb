ca2d7037e95e8b66d1a7ca991a032aeddd45d7c1
ayumi nakano <aynminoa@gmail.com>
2022-09-20 13:50:41 +0900

    全ユーザーの詳細画面に同じアルバムが表示される問題を解消、戻る・編集を押したときの遷移調整、user/albumモデルにdependent: :destroy追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @albums = Album.where(user_id: @user.id)
     @album = @user.albums.build
-    @albums = Album.all
   end
 
   def destroy
