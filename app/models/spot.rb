d1ed94ea7e9ea326f45c2bb2c791ade28a350d12
ayumi nakano <aynminoa@gmail.com>
2022-09-22 12:01:53 +0900

    tagのCRUD, spot_tagモデル作成、spot/tag/spot_tagモデルに関連付け指定

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :album
+  has_many :spot_tags, dependent: :destroy
+  has_many :tags, through: :spot_tags
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
---
+++ app/models/tag.rb
@@ -0,0 +1,4 @@
+class Tag < ApplicationRecord
+  has_many :spot_tags, dependent: :destroy
+  has_many :spots, through: :spot_tags
+end
