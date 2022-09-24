aa07c0076513b72b4038faa103b30d73d260aaf8
ayumi nakano <aynminoa@gmail.com>
2022-09-24 16:55:51 +0900

    ゲストログイン機能設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   #ActiveStorage
   has_one_attached :image
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲスト"
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
 end
