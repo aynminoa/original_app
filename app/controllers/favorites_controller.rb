952e9ff5be4637e6bb379836b3c0256d86aa0c3d
ayumi nakano <aynminoa@gmail.com>
2022-09-28 09:57:08 +0900

    spot/showとalbum/showでお気に入りを同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,25 @@
 class FavoritesController < ApplicationController
+
   def create
     favorite = current_user.favorites.create(spot_id: params[:spot_id])
-    redirect_to spot_path(params[:spot_id]), notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り登録しました"
+    show_of_album_or_spot
+    # redirect_to spot_path(params[:spot_id]), notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:favorite_id]).destroy
-    redirect_to spot_path, notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り解除しました"
+    show_of_album_or_spot
+    # redirect_to spot_path, notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り解除しました"
   end
+
+  private
+
+  def show_of_album_or_spot
+    if params[:album_show]
+      redirect_to album_path(params[:id])
+    else
+      redirect_to spot_path(params[:spot_id])
+    end
+  end
+
 end
