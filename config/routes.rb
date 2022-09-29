44d38f4913e55af99a733279967665696443b552
ayumi nakano <aynminoa@gmail.com>
2022-09-29 17:20:26 +0900

    バリデーションエラーメッセージ表示の設定・修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'homes#index'
   resources :favorites, only: %i[create destroy]
-  resources :tags
-  resources :spots
   devise_for :users, controllers: { 
     sessions: 'users/sessions',
     registrations: 'users/registrations' }
-  
+    
     devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
@@ -15,4 +13,6 @@ Rails.application.routes.draw do
   
   resources :users, only: %i[index show destroy]
   resources :albums
+    resources :tags
+    resources :spots
 end
