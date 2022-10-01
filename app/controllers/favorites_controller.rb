16b9f88ac27ab46dfac1dd1cf53d33784b75024d
ayumi nakano <aynminoa@gmail.com>
2022-10-01 21:21:11 +0900

    不要なコード削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,16 +6,11 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    # binding.pry
     @favorite = current_user.favorites.create(spot_id: params[:spot_id])
-    # show_of_album_or_spot
-    # redirect_to spot_path(params[:spot_id]), notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り登録しました"
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    # show_of_album_or_spot
-    # redirect_to spot_path, notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り解除しました"
   end
 
   private
@@ -23,12 +18,5 @@ class FavoritesController < ApplicationController
   def find_spot
     @spot = Spot.find(params[:spot_id])
   end
-  # def show_of_album_or_spot
-  #   if params[:album_show]
-  #     redirect_to album_path(params[:id])
-  #   else
-  #     redirect_to spot_path(params[:spot_id])
-  #   end
-  # end
 
 end
