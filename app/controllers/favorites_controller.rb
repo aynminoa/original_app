c09350be4d89ad8435cfc2d34cbbd788e06101c0
ayumi nakano <aynminoa@gmail.com>
2022-10-01 21:14:13 +0900

    spot/showとalbum/showのお気に入り機能をAjax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,29 +1,34 @@
 class FavoritesController < ApplicationController
+  before_action :find_spot
 
   def index
     @favorite_spots = current_user.favorite_spots
   end
 
   def create
-    favorite = current_user.favorites.create(spot_id: params[:spot_id])
-    show_of_album_or_spot
+    # binding.pry
+    @favorite = current_user.favorites.create(spot_id: params[:spot_id])
+    # show_of_album_or_spot
     # redirect_to spot_path(params[:spot_id]), notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り登録しました"
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:favorite_id]).destroy
-    show_of_album_or_spot
+    current_user.favorites.find_by(id: params[:id]).destroy
+    # show_of_album_or_spot
     # redirect_to spot_path, notice: "#{favorite.spot.album.user.name}さんのspotをお気に入り解除しました"
   end
 
   private
 
-  def show_of_album_or_spot
-    if params[:album_show]
-      redirect_to album_path(params[:id])
-    else
-      redirect_to spot_path(params[:spot_id])
-    end
+  def find_spot
+    @spot = Spot.find(params[:spot_id])
   end
+  # def show_of_album_or_spot
+  #   if params[:album_show]
+  #     redirect_to album_path(params[:id])
+  #   else
+  #     redirect_to spot_path(params[:spot_id])
+  #   end
+  # end
 
 end
