a113353f24d2f7092ea0d403d1e4e6123f060550
ayumi nakano <aynminoa@gmail.com>
2022-10-13 20:25:48 +0900

    検証

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,12 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: 'dicayumi@gmail.com'
+    user_name: 'dicayumi@gmail.com',
     # user_name: Rails.application.credentials.gmail[:user_name],  #Gmailアドレス（credentials.yml.encに記載）
     password: Rails.application.credentials.gmail[:password],  #アプリパスワード（credentials.yml.encに記載）
     # user_name: ENV['SEND_MAIL'],
     # password: ENV['GMAIL_SPECIFIC_PASSWORD'],
-    authentication: :login
+    authentication: 'login'
   }
 
   # Don't care if the mailer can't send.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,12 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: 'dicayumi@gmail.com'
+    user_name: 'dicayumi@gmail.com',
     # user_name: Rails.application.credentials.gmail[:user_name],  #Gmailアドレス（credentials.yml.encに記載）
     password: Rails.application.credentials.gmail[:password],  #アプリパスワード（credentials.yml.encに記載）
     # user_name: ENV['SEND_MAIL'],
     # password: ENV['GMAIL_SPECIFIC_PASSWORD'],
-    authentication: :login,
+    authentication: 'login',
     openssl_verify_mode: 'none',
     enable_starttls_auto: true
   }
