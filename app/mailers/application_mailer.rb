30be902c95fdb0bc888a87c8713da74c3009df53
ayumi nakano <aynminoa@gmail.com>
2022-10-13 20:20:29 +0900

    検証

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'dicayumi@gmail.com'
-          # reply_to: Rails.application.credentials.gmail[:user_name]
-          # reply_to: 'dicayumi@gmail.com'
   layout 'mailer'
 end
