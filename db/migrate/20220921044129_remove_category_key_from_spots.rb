11f853b05998d6a840a0b63d0ae31549a9b36cde
ayumi nakano <aynminoa@gmail.com>
2022-10-13 16:18:59 +0900

    migrationファイル１部削除

--- db/migrate/20220921044129_remove_category_key_from_spots.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCategoryKeyFromSpots < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :spots, :categories
-    remove_reference :spots, :category, index: true
-  end
-end
