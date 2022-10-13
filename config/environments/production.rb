7a45e050c02b64693907b7f1d15c2e6961283ee9
ayumi nakano <aynminoa@gmail.com>
2022-10-13 17:52:29 +0900

    envに追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,23 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "original_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = {  host: 'http://52.194.95.114/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    # user_name: Rails.application.credentials.gmail[:user_name],  #Gmailアドレス（credentials.yml.encに記載）
+    # password: Rails.application.credentials.gmail[:password],  #アプリパスワード（credentials.yml.encに記載）
+    user_name: ENV['SEND_MAIL'],
+    password: ENV['GMAIL_SPECIFIC_PASSWORD'],
+    authentication: :login,
+    openssl_verify_mode: 'none',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
